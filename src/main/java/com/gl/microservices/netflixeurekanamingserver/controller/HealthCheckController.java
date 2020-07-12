152bca30beee634c7efd9fcdd2a098c1d2c7fa4a
Biswajit Mohapatra <biswajit.javaexpert@gmail.com>
2020-07-12 18:12:12 +0530

    Lets make sure app is working

--- src/main/java/com/gl/microservices/netflixeurekanamingserver/controller/HealthCheckController.java
+++ src/main/java/com/gl/microservices/netflixeurekanamingserver/controller/HealthCheckController.java
@@ -1,5 +1,15 @@
 package com.gl.microservices.netflixeurekanamingserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "healthcheck")
 public class HealthCheckController {
 
+  @GetMapping(value = "")
+  public String status(){
+    return "GREEN";
+  }
 }
